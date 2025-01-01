424211271a3905170d1ca2e60afd57b9740ecb2c
Mo <vtme996@gmail.com>
2025-01-01 16:14:54 -0600

    first commit

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utils"
+import type { HTMLAttributes, ReactNode } from "react"
+
+interface Props extends HTMLAttributes<HTMLHeadingElement> {
+  children?: ReactNode
+}
+export function Heading({ children, className, ...props }: Props) {
+  return (
+    <h1
+      className={cn(
+        "text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zing-800",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  )
+}
---
+++ src/app/components/max-width-wrapper.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utils"
+import type { ReactNode } from "react"
+
+interface Props {
+  className?: string
+  children: ReactNode
+}
+export function MaxWidthWrapper({ className, children }: Props) {
+  return (
+    <div
+      className={cn(
+        "h-full w-full mx-auto max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
